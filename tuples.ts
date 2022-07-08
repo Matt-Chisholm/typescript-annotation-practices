0c7871662b029a6a932d5f2f87881d6d28c5455c
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-07-08 10:50:45 -0700

    cool

--- tuples.ts
+++ tuples.ts
@@ -4,4 +4,10 @@ const drink = {
     sugar: 40
 };
 
-const pepsi: [string, boolean, number] = ['brown', true, 40];
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
+
+const sprite: Drink = ['clear', true, 35];
+
+const tea: Drink = ['brown', false, 0];
