01888b972e36de163d916909e18b0b9eb4b15cb0
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-08-11 11:42:38 -0700

    generic constraints

--- generics.ts
+++ generics.ts
@@ -41,3 +41,27 @@ function printAnything<T>(arr: T[]): void {
         console.log(arr[i]);
     }
 }
+
+// Generic Constraints
+
+class Car {
+    print() {
+        console.log('i am a car!');
+    }
+}
+
+class House {
+    print() {
+        console.log('i am a house!');
+    }
+}
+
+interface Printable {
+    print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].print();
+    }
+}
